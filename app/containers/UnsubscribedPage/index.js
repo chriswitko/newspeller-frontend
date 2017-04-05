11aa51661fa7239a663e68db3b74aa68afe7f6e6
Chris Witko <chris.witko@gmail.com>
2017-04-05 11:35:01 +0100

    fix unsubscribe page

--- app/containers/UnsubscribedPage/index.js
+++ app/containers/UnsubscribedPage/index.js
@@ -6,13 +6,8 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-import H1 from 'components/H1'
-import Wrapper from 'components/Wrapper'
-import Ul from './Ul'
-import Header from 'components/Header'
-import Footer from 'components/Footer'
-import { Page, Row, Column } from 'hedron'
-import Box from 'components/Box'
+import { Row, Col } from 'react-grid-system'
+import SpaceWrapper from 'components/SpaceWrapper'
 
 export default class UnsubscribePage extends React.Component {
   // Since state and props are static,
@@ -23,51 +18,20 @@ export default class UnsubscribePage extends React.Component {
 
   render () {
     return (
-      <Box fullScreen>
-        <Page style={{display: 'flex', flexDirection: 'column'}}>
-          <Row>
-            <Header />
-          </Row>
-          <Row style={{flex: 1}}>
-            <Column>
-              <article>
-                <div>
-                  <Helmet
-                    title='Unsubscribe'
-                    meta={[
-                      { name: 'description', content: 'Feature page of React.js Boilerplate application' }
-                    ]}
-                  />
-                  <H1>
-                    Unsubscribe successful
-                  </H1>
-                  <Wrapper>
-                    <div>
-                      You have been removed from the Newspeller email.
-                    </div>
-                    <br />
-                    <div>
-                      <strong>If you have a moment, please let us know why you unsubscribed:</strong>
-                    </div>
-                    <div>
-                      <Ul>
-                        <li><input type='radio' /> I no longer want to receive these emails</li>
-                        <li><input type='radio' /> I never signed up for this email</li>
-                        <li><input type='radio' /> These emails are inappropriate</li>
-                        <li><input type='radio' /> These emails are spam and should be reported</li>
-                        <li><input type='radio' /> I don't like ads within email</li>
-                      </Ul>
-                    </div>
-                  </Wrapper>
-                </div>
-              </article>
-            </Column>
-          </Row>
-          <Row alignSelf='flex-start' style={{width: '100%'}}>
-            <Footer />
-          </Row>
-        </Page>
-      </Box>
+      <div>
+        <Row>
+          <Col>
+            <article>
+              <SpaceWrapper>
+                <Helmet
+                  title='Unsubscribe'
+                />
+                Unsubscribe successful
+              </SpaceWrapper>
+            </article>
+          </Col>
+        </Row>
+      </div>
     )
   }
 }
